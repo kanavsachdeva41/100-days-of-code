819cd3d4a6ae131b8d4f943c1fdeba359b81502b
kanavsachdeva41 <kanavsachdeva41@gmail.com>
2025-10-14 23:54:26 +0530

    question

---
+++ DAY29Q58.c
@@ -0,0 +1,52 @@
+// Q58: Find the maximum and minimum element in an array. //
+
+/*
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+
+*/
+
+#include <stdio.h>
+void main()
+{
+    int n,max,min;
+    printf("Enter index of specified array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+
+    printf("enter value of array: ");
+
+    for(int i=0; i<n; i++)
+    {
+       scanf("%d", &arr[i]);
+    }    
+
+    max=arr[0];
+    min=arr[0];
+
+    for (int i=1;i<n;i++)
+    {
+        if (arr[i]>max)
+       {
+        max=arr[i];
+       }
+       if (arr[i]<min)
+       {
+        min=arr[i];
+       }
+    }
+    printf("Max is %d\n", max);
+    printf("Min is %d", min);
+    
+}
