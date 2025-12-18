29b7a3ca15e0db69dd40ef0f930bd2f754a5c2c8
kanavsachdeva41 <kanavsachdeva41@gmail.com>
2025-12-18 15:52:33 +0530

    question

---
+++ DAY62Q112.c
@@ -0,0 +1,58 @@
+// Q112: Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element. //
+
+/*
+Sample Test Cases:
+Input 1:
+arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output 1:
+11
+
+Input 2:
+arr[] = [-2, -4]
+Output 2:
+-2
+
+Input 3:
+arr[] = [5, 4, 1, 7, 8]
+Output 3:
+25
+
+*/
+#include <stdio.h>
+void main()
+{
+    int n,maxsum,currsum;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    currsum=arr[0];
+    maxsum=arr[0];
+
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>currsum+arr[i])
+        {
+            currsum=arr[i];
+        }
+        else
+        {
+            currsum+=arr[i];
+        }
+    }
+
+    if(currsum>maxsum)
+    {
+        maxsum=currsum;
+    }
+
+    printf("Max sum: %d\n",maxsum);
+
+}
