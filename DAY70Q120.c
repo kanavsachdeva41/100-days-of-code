c8de76ff591e7761304e25a87b1062ef14287c84
kanavsachdeva41 <kanavsachdeva41@gmail.com>
2026-01-01 16:09:15 +0530

    question

---
+++ DAY70Q120.c
@@ -0,0 +1,42 @@
+// Q120: Write a program to take a string input. Change it to sentence case. //
+
+/*
+Sample Test Cases:
+Input 1:
+str = I am trying to build logic.
+Output 1:
+I Am Trying To Build Logic
+
+Input 2:
+str = The classes are supposed to start early.
+Output 2:
+The Classes Are Supposed To Start Early.
+
+Input 3:
+str = We are going to look at 26 different test cases.
+Output 3:
+We Are Going To Look At 26 Different Test Cases.
+
+*/
+#include <stdio.h>
+void main()
+{
+    char str[100];
+    
+    printf("Enter string: ");
+    fgets(str,100,stdin);
+
+    for(int i=0;str[i] != '\0'; i++)
+    {
+        if(i==0 && str[i] != ' ')
+        {
+            str[i]=str[i]-32;
+        }
+        if(str[i-1]==' ' && str[i]!=' ')
+        {
+            str[i]=str[i]-32;
+        }
+    }
+
+    puts(str);
+}
